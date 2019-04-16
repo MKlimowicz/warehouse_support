5f8e3a0fa406258a5dfd4e1e889397cc6f5bb72f
Marcin Kilimo <mklimowicz96@wp.pl>
2019-04-16 18:12:24 +0200

    modified Employee

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,9 +1,45 @@
 package model;
 
 public class Employee {
-    int id;
+    private int id;
+    private String name;
+    private String lastName;
 
-    
+    public Employee(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
